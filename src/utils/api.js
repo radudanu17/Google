a963cba97cfb87ab403aed48f31d054f236ec990
Danu Rodion <danurodion@gmail.com>
2023-09-24 22:05:46 +0300

    All

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,13 +3,13 @@ import axios from "axios";
 const BASE_URL = "https://www.googleapis.com/customsearch/v1";
 
 const params = {
-    key: "YOUR_API_KEY",
-    cx: "YOUR_CX_KEY",
+  key: "AIzaSyB8cEJBO2zaOUA6rlvAhThM0MU_XtjMpjk",
+  cx: "d36f93a549ccc4b15",
 };
 
 export const fetchDataFromApi = async (payload) => {
-    const { data } = await axios.get(BASE_URL, {
-        params: { ...params, ...payload },
-    });
-    return data;
+  const { data } = await axios.get(BASE_URL, {
+    params: { ...params, ...payload },
+  });
+  return data;
 };
